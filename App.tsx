250777785a523e2958900ebfe58bbaa1bfe375ac
Usamaa <usamabadarbasra@gmail.com>
2024-07-28 04:48:15 -0700

    updated code

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Login from './screens/login/login';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Login />
+      </View>
+    );
+  }
+}
